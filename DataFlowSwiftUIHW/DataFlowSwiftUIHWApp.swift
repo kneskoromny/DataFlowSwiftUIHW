f5bf49e2a56a4f7dec35d60c89c98ab955d2add2
kneskoromny <kneskoromny@gmail.com>
2021-06-09 12:12:24 +0300

    Initial Commit

---
+++ DataFlowSwiftUIHW/DataFlowSwiftUIHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowSwiftUIHWApp.swift
+//  DataFlowSwiftUIHW
+//
+//  Created by Кирилл Нескоромный on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowSwiftUIHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
